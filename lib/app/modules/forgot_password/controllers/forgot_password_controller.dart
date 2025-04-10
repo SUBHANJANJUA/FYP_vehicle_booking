bab786f82cf13c8f61d6238c03035ca70ddbac7c
SUBHANJANJUA <subhanjanjua837@gmail.com>
2025-04-10 10:54:12 +0500

    forgot password

--- lib/app/modules/forgot_password/controllers/forgot_password_controller.dart
+++ lib/app/modules/forgot_password/controllers/forgot_password_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class ForgotPasswordController extends GetxController {
   var obsecure1 = true.obs;
@@ -9,4 +10,33 @@ class ForgotPasswordController extends GetxController {
   final TextEditingController newPasswordController = TextEditingController();
   final TextEditingController confirmPasswordController =
       TextEditingController();
+
+  void sendResetEmail() async {
+    try {
+      await FirebaseAuth.instance.sendPasswordResetEmail(
+        email: emailController.text.trim(),
+      );
+      Get.snackbar(
+        "Success",
+        "Password reset email sent!",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.green,
+        colorText: Colors.white,
+      );
+    } on FirebaseAuthException catch (e) {
+      Get.snackbar(
+        "Error",
+        e.message ?? "An error occurred",
+        snackPosition: SnackPosition.BOTTOM,
+        backgroundColor: Colors.red,
+        colorText: Colors.white,
+      );
+    }
+  }
+
+  @override
+  void onClose() {
+    emailController.dispose();
+    super.onClose();
+  }
 }
--- lib/app/modules/forgot_password/views/forgot_password_view.dart
+++ lib/app/modules/forgot_password/views/forgot_password_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 import 'package:vehicle_booking/app/data/util/heading16Green.dart';
 import 'package:vehicle_booking/app/data/util/signup_row.dart';
 import 'package:vehicle_booking/app/modules/forgot_password/views/otp_view.dart';
+import 'package:vehicle_booking/app/modules/signup/views/signin_view.dart';
 import 'package:vehicle_booking/gen/assets.gen.dart';
 import '../../../data/app_colors.dart';
 import '../controllers/forgot_password_controller.dart';
@@ -80,11 +81,12 @@ class ForgotPasswordView extends GetView<ForgotPasswordController> {
                       child: ElevatedButton(
                           onPressed: () {
                             if (formKey.currentState!.validate()) {
-                              Get.to(() => OtpView());
+                              controller.sendResetEmail();
+                              Get.offAll(SignInView());
                             }
                           },
                           child: Text(
-                            'Next',
+                            'Sign In',
                             style: TextStyle(color: AppColors.white),
                           )))
                 ],
